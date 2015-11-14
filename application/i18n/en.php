1cb85d6e8f533a4d40794716ed36535259b9c403
arwutang <arwutang@hotmail.com>
2015-11-14 23:58:15 +0800

    wording change of web code

--- application/i18n/en.php
+++ application/i18n/en.php
@@ -3,12 +3,12 @@
 return array(
     'not_login' => 'not login',
     'logged_in' => 'logged in',
-    'image_err' => 'incorrect image',
+    'image_err' => 'validation code is not correct',
     'register_failed' => 'register failed',
     'register_success' => 'register success',
     'upload_failed' => 'upload failed',
     'upload_success' => 'upload success',
     'email_existed' => 'email existed',
-    'brand_existed' => 'brand existed',
+    'brand_existed' => 'brand name has already been resisted',
     'check_ok' => 'ok',
 );
