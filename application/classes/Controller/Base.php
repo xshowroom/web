64a67cc8ad93a826db70f721390191f5e253f6a7
liys29 <yoyo.90@qq.com>
2015-11-14 16:16:06 +0800

    i18n

--- application/classes/Controller/Base.php
+++ application/classes/Controller/Base.php
@@ -4,14 +4,18 @@
 class Controller_Base extends Controller
 {
 
+    const MSG_KEY = 'base_1';
+    
 	public function before()
 	{
 	    session_start();
 	    $opUser = $_SESSION['opUser'];
+        I18n::lang($_COOKIE['language']);
+	    
 		if (empty($opUser)) {
 			echo json_encode(array(
                 'status' => LOGIN_FAILURE,
-                'msg' 	 => 'not login',
+                'msg' 	 => __(MSG_KEY),
             ));
 		}
 	}
