f53f76a280f014a0184b08d99aea36620df1ad20
Shi-Liang <385936547@qq.com>
2015-11-17 19:58:52 +0800

    front-end 样式修正

--- www/application/views/common/global_setting_without_login.php
+++ www/application/views/common/global_setting_without_login.php
@@ -1,7 +1,7 @@
 <div class="container">
 	<div class="row">
 		<div class="col-xs-12 text-right">
-			<div class="locale-settings"></div>
+			<div class="locale-settings right"></div>
 		</div>
 	</div>
 </div>
