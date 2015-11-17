e8c30198c6d234cb1458b7454ac4690f28d14eab
arwutang <arwutang@hotmail.com>
2015-11-18 03:07:01 +0800

    translation work

--- www/application/views/common/global_navigation_top_guest.php
+++ www/application/views/common/global_navigation_top_guest.php
@@ -16,12 +16,12 @@
 	<div class="row">
 		<div class="col-xs-12 text-center">
 			<ul class="main-naviagtions">
-				<li><a href="/home" <?php if ($currentPage == 'home'){?>class="active"<?php }?>>{{ "global_no_user_navigation__HOME"| translate }}</a></li>
-				<li><a href="/guide" <?php if ($currentPage == 'guide'){?>class="active"<?php }?>>{{ "global_no_user_navigation__GUIDE"| translate }}</a></li>
-				<li><a href="/shop" <?php if ($currentPage == 'shop'){?>class="active"<?php }?>>{{ "global_no_user_navigation__SHOP"| translate }}</a></li>
-				<li><a href="#"  <?php if ($currentPage == 'discovery'){?>class="active"<?php }?>>{{ "global_no_user_navigation__DISCOVER"| translate }}</a></li>
-				<li><a href="#"  <?php if ($currentPage == 'press'){?>class="active"<?php }?>>{{ "global_no_user_navigation__PRESS"| translate }}</a></li>
-				<li><a href="#"  <?php if ($currentPage == 'contact'){?>class="active"<?php }?>>{{ "global_no_user_navigation__CONTACT"| translate }}</a></li>
+				<li><a href="/home" <?php if ($currentPage == 'home'){?>class="active"<?php }?>><?= __("global_navigation_top_guest__HOME") ?></a></li>
+				<li><a href="/guide" <?php if ($currentPage == 'guide'){?>class="active"<?php }?>><?= __("global_navigation_top_guest__GUIDE") ?></a></li>
+				<li><a href="/shop" <?php if ($currentPage == 'shop'){?>class="active"<?php }?>><?= __("global_navigation_top_guest__SHOP") ?></a></li>
+				<li><a href="#"  <?php if ($currentPage == 'discovery'){?>class="active"<?php }?>><?= __("global_navigation_top_guest__DISCOVER") ?></a></li>
+				<li><a href="#"  <?php if ($currentPage == 'press'){?>class="active"<?php }?>><?= __("global_navigation_top_guest__PRESS") ?></a></li>
+				<li><a href="#"  <?php if ($currentPage == 'contact'){?>class="active"<?php }?>><?= __("global_navigation_top_guest__CONTACT") ?></a></li>
 			</ul>
 		</div>
 	</div>
