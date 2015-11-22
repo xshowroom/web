ce8fc78b1073fc849e8488beb1110d3ebbbaedd0
Shi-Liang <385936547@qq.com>
2015-11-22 21:04:02 +0800

    bug fix

--- www/application/views/collection_index.php
+++ www/application/views/collection_index.php
@@ -6,13 +6,17 @@
     <link rel="shortcut icon" href="/favicon.ico" />
     <link rel="stylesheet" type="text/css" href="/static/bower_components/bootstrap/dist/css/bootstrap.min.css" />
     <link rel="stylesheet" type="text/css" href="/static/bower_components/font-awesome/css/font-awesome.min.css" />
+    <link rel="stylesheet" type="text/css" href="/static/bower_components/angular-motion/dist/angular-motion.min.css">
     <link rel="stylesheet" type="text/css" href="/static/app/css/common.css" />
     <link rel="stylesheet" type="text/css" href="/static/app/css/collection_index.css" />
     <link rel="stylesheet" type="text/css" href="/static/app/css/collection_create.css" />
     <script type="text/javascript" src="/static/bower_components/jquery/dist/jquery.min.js"></script>
     <script type="text/javascript" src="/static/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="/static/bower_components/angular/angular.min.js"></script>
+    <script type="text/javascript" src="/static/bower_components/angular-animate/angular-animate.min.js"></script>
     <script type="text/javascript" src="/static/bower_components/angular-cookies/angular-cookies.min.js"></script>
+    <script type="text/javascript" src="/static/bower_components/angular-strap/dist/angular-strap.min.js"></script>
+    <script type="text/javascript" src="/static/bower_components/angular-strap/dist/angular-strap.tpl.min.js"></script>
     <script type="text/javascript" src="/static/app/modules/common/i18n.js"></script>
     <script type="text/javascript" src="/static/app/modules/common/services.js"></script>
     <script type="text/javascript" src="/static/app/modules/common/directives.js"></script>
