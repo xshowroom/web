d3a22faaa035281f10bfbb44818efd4fe10c5fd5
Shi-Liang <385936547@qq.com>
2015-11-24 21:05:56 +0800

    front-end
    brand all collections基本完成

--- www/application/views/brand_collection.php
+++ www/application/views/brand_collection.php
@@ -34,27 +34,15 @@
                 	<h3>STATUS</h3>
                 	<ul>
                 		<li>
-                			<a ng-click="filter.status = ''; filter.limit = 2;">
+                			<a ng-click="filters.status = ''; filters.limit = 4;">
                 				<span>{{'COLLECTION_STATUS_ALL' | translate}}</span>
                 				<span>{{collections.length || 0}}</span>
                 			</a>
                 		</li>
-                		<li ng-if="statusCounter[0]">
-                			<a ng-click="filter.status = '0'; filter.limit = 2;">
-                				<span>{{'COLLECTION_STATUS_0' | translate}}</span>
-                				<span>{{statusCounter[0] || 0}}</span>
-                			</a>
-                		</li>
-                		<li ng-if="statusCounter[1]">
-                			<a ng-click="filter.status = '1'; filter.limit = 2;">
-                				<span>{{'COLLECTION_STATUS_1' | translate}}</span>
-                				<span>{{statusCounter[1] || 0}}</span>
-                			</a>
-                		</li>
-                		<li ng-if="statusCounter[2]">
-                			<a ng-click="filter.status = '2';">
-                				<span>{{'COLLECTION_STATUS_2' | translate}}</span>
-                				<span>{{statusCounter[2] || 0}}</span>
+                		<li ng-repeat="(status,count) in statusCounter">
+                			<a ng-click="filters.status = status; filters.limit = 4;">
+                				<span>{{('COLLECTION_STATUS_' + status) | translate}}</span>
+                				<span>{{count}}</span>
                 			</a>
                 		</li>
                 	</ul>
@@ -63,7 +51,7 @@
                 	</div>
                 </div>
             	<div class="col-xs-9">
-            		<div class="collection-item" ng-repeat="collection in collections| filter: {status: filter.status} | limitTo : filter.limit : 0 ">
+            		<div class="collection-item" ng-repeat="collection in collections| filter: {status: filters.status} | limitTo : filters.limit : 0 ">
             			<h3>{{collection.name| uppercase}} {{collection.category | translate}} (87)</h3>
             			<div class="collection-info">
             				<div class="col-xs-4 collection-cover">
@@ -82,27 +70,30 @@
 			                 	</div>
 			                 	<div class="col-xs-12 collection-actions">
 			                 		<a class="btn btn-type-2" href="/collection/{{collection.id}}" target="_self">VIEW</a>
-			                 		<button class="btn btn-type-1" ng-click="enableCollection(collection.id);" ng-if="collection.status == '0'">SUBMIT</button>
+			                 		<!-- <button class="btn btn-type-1" ng-click="enableCollection(collection.id);" ng-if="collection.status == '0'">SUBMIT</button>
 			                 		<button class="btn btn-type-1" ng-click="deleteCollection(collection.id);" ng-if="collection.status == '0'">DELETE</button>
-			                 		<button class="btn btn-type-1" ng-click="closeCollection(collection.id);"  ng-if="collection.status == '1'">CLOSE</button>
+			                 		<button class="btn btn-type-1" ng-click="closeCollection(collection.id);"  ng-if="collection.status == '1'">CLOSE</button> -->
 			                 	</div>
             				</div>
             			</div>
-            			<div class="product-gallery">
-            				<div class="col-xs-1 product-gallery-action">
-            					<span class="glyphicon glyphicon-chevron-left"></span>
+            			<div class="product-gallery" ng-init="collection.offset = 0;" ng-if="collection.productions.length">
+            				<div class="product-gallery-action" ng-class="{'disabled': collection.offset == 0}">
+            					<span class="glyphicon glyphicon-chevron-left" ng-click="collection.offset = collection.offset - (collection.offset > 0 ? 1 : 0)"></span>
             				</div>
-            				<div class="col-xs-10 product-list">
-            					<div class="product-list-preview">
-            						<img src="">
-            					
+            				<div class="product-list-preview">
+            					<div class="product-image" ng-repeat="product in collection.productions | limitTo: 5 : collection.offset">
+            						<img ng-src="/{{product.image_url[0]}}">
             					</div>
             				</div>
-            				<div class="col-xs-1 product-gallery-action">
-            					<span class="glyphicon glyphicon-chevron-right"></span>
+            				<div class="product-gallery-action" ng-class="{'disabled': collection.offset == collection.productions.length - 5 || collection.productions.length <= 5}">
+            					<span class="glyphicon glyphicon-chevron-right" ng-click="collection.offset = collection.offset + ((collection.offset < collection.productions.length - 5) ? 1 : 0);"></span>
             				</div>
             			</div>
             		</div>
+            		<div class="clearfix"></div>
+                 	<div class="text-center load-more" ng-if="filters.limit < (statusCounter[filters.status] || collections.length)">
+                 		<button class="btn btn-type-1" ng-click="filters.limit = filters.limit + 4;">LOAD MORE</button>
+                 	</div>
             	</div>
 			</div>
 		</div>
