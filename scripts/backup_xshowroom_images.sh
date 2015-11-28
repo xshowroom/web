df48dc95ecdcffbfd9a40586b47206824a476af6
arwutang <arwutang@hotmail.com>
2015-11-29 00:59:51 +0800

    update scripts

--- scripts/backup_xshowroom_images.sh
+++ scripts/backup_xshowroom_images.sh
@@ -1,12 +1,12 @@
 images_file_path=/home/dev_root/xshowroom/web/www/data
 backup_file_name=/home/tangxiaotao/workspace/xshowroom/database_backup/full_image_bak_$(date -d today +"%Y_%-m_%-d-%H_%M_%S")
 
-echo 'full backup xshowroom images to file: '$backup_file_name.gz
+echo 'full backup xshowroom images to file: '$backup_file_name.zip
 
 cp -r $images_file_path  $backup_file_name
 rm -rf $backup_file_name/tmp
 
-tar -zcvf $backup_file_name.gz $backup_file_name
+zip -r $backup_file_name.zip $backup_file_name
 rm -rf $backup_file_name
 
 echo 'backup images success'
