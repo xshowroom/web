51c787bc035c51c7ce1a7c658875bba5ed6425f2
pepeshen <shenpeipei@192.168.0.105>
2015-11-30 21:51:49 +0800

    guest访问brand/list权限

--- www/application/classes/Controller/Api/Brand.php
+++ www/application/classes/Controller/Api/Brand.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Controller_Api_Brand extends Controller_BaseReqLogin
+class Controller_Api_Brand extends Controller_Base
 {
     public $brandService;
 
