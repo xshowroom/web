f7f5c2a40b1d0cfaae34beb5999c603890f59969
arwutang@hotmail.com <arwutang@hotmail.com>
2015-11-30 11:37:50 +0800

    missing translation

--- www/application/views/product_create.php
+++ www/application/views/product_create.php
@@ -215,7 +215,7 @@
 						<ul>
 							<li ng-repeat="msg in colorErrorMsg">
 								<span class="glyphicon glyphicon-remove-sign"></span>
-								<span>#{{msg.index + 1}} {{('product_add_image_' + msg.msg) }}</span>
+								<span>#{{msg.index + 1}} {{('product_add_image_' + msg.msg) | translate }}</span>
 							</li>
 						</ul>
 						<div class="clearfix"></div>
