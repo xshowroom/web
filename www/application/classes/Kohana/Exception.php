9283e93a09892fa64ed13663543e61da0d69bb95
arwutang@hotmail.com <arwutang@hotmail.com>
2015-12-09 20:14:15 +0800

    inject user info in error msg

--- www/application/classes/Kohana/Exception.php
+++ www/application/classes/Kohana/Exception.php
@@ -41,8 +41,17 @@ class Kohana_Exception extends Kohana_Kohana_Exception
             $errorMsg = '您所查找的页面不存在';
         }
         $errorMsg = HTML::entities($errorMsg);
-        
+
+        $opUser = $_SESSION['opUser'];
+
         $view = View::factory('errors/error_msg');
+
+        if(!empty($opUser)) {
+            $view->set('user', $opUser);
+            $userService = new Business_User();
+            $view->set('userAttr', $userService->getUserAttr($opUser['id']));
+        }
+
         $view->set('errorCode', $errorCode);
         $view->set('errorMsg', $errorMsg);
         
