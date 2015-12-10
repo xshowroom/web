edc59ed6718d5d9522f56a649a3c8bd6f452929d
arwutang <arwutang@hotmail.com>
2015-12-11 02:57:10 +0800

    add store in nav

--- www/application/views/common/global_navigation_top_buyer.php
+++ www/application/views/common/global_navigation_top_buyer.php
@@ -8,9 +8,10 @@
 		<div class="col-xs-7">
 			<ul class="main-naviagtions">
 				<li><a href="<?=URL::site('buyer/dashboard');?>" <?php if ($currentPage == 'dashboard'){?>class="active"<?php }?>><?=__("global_navigation_top_user__DASHBOARD");?></a></li>
-				<li><a href="<?=URL::site('shop');?>" <?php if ($currentPage == 'shop'){?>class="active"<?php }?>><?=__("global_navigation_top_user__SHOP");?></a></li>
+				<li><a href="<?=URL::site('shop');?>" <?php if ($currentPage == 'shop'){?>class="active"<?php }?>><?=__("global_navigation_top_user__BRAND");?></a></li>
 				<li><a href="<?=URL::site('buyer/order');?>" <?php if ($currentPage == 'order'){?>class="active"<?php }?>><?=__("global_navigation_top_user__ORDER");?></a></li>
 				<li><a href="#" <?php if ($currentPage == 'cart'){?>class="active"<?php }?>><?=__("global_navigation_top_user__CART");?></a></li>
+				<li><a href="<?=URL::site('buyer/store');?>" <?php if ($currentPage == 'shop'){?>class="active"<?php }?>><?=__("global_navigation_top_user__STORE");?></a></li>
 				<li><a href="<?=URL::site('message/list');?>"  <?php if ($currentPage == 'message'){?>class="active"<?php }?>><?=__("global_navigation_top_user__MESSAGE");?></a></li>
 			</ul>
 		</div>
