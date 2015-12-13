573bb4c66f9c9dec9b8a9900bbec2bb6b67b3088
Shi-Liang <385936547@qq.com>
2015-12-13 21:06:31 +0800

    code style

--- www/application/views/order_create.php
+++ www/application/views/order_create.php
@@ -2,38 +2,25 @@
 <html ng-app="xShowroom.order.create"
 	ng-init="collectionId=<?=$collection['id']?>; minOrder=<?=$collection['mini_order']?>;">
 <head>
-<meta charset="UTF-8">
-<title>XShowroom</title>
-<link rel="stylesheet" type="text/css"
-	href="/static/bower_components/bootstrap/dist/css/bootstrap.min.css" />
-<link rel="stylesheet" type="text/css"
-	href="/static/bower_components/font-awesome/css/font-awesome.min.css" />
-<link rel="stylesheet" type="text/css"
-	href="/static/bower_components/angular-motion/dist/angular-motion.min.css">
-<link rel="stylesheet" type="text/css" href="/static/app/css/common.css" />
-<link rel="stylesheet" type="text/css"
-	href="/static/app/css/order_create.css" />
-<link rel="shortcut icon" href="/favicon.ico" />
-<script type="text/javascript"
-	src="/static/bower_components/jquery/dist/jquery.min.js"></script>
-<script type="text/javascript"
-	src="/static/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<script type="text/javascript"
-	src="/static/bower_components/angular/angular.min.js"></script>
-<script type="text/javascript"
-	src="/static/bower_components/angular-cookies/angular-cookies.min.js"></script>
-<script type="text/javascript"
-	src="/static/bower_components/angular-sanitize/angular-sanitize.min.js"></script>
-<script type="text/javascript"
-	src="/static/bower_components/angular-strap/dist/angular-strap.min.js"></script>
-<script type="text/javascript"
-	src="/static/bower_components/angular-strap/dist/angular-strap.tpl.min.js"></script>
-<script type="text/javascript" src="/static/app/modules/common/i18n.js"></script>
-<script type="text/javascript"
-	src="/static/app/modules/common/services.js"></script>
-<script type="text/javascript"
-	src="/static/app/modules/common/directives.js"></script>
-<script type="text/javascript" src="/static/app/modules/order_create.js"></script>
+	<meta charset="UTF-8">
+	<title>XShowroom</title>
+	<link rel="stylesheet" type="text/css" href="/static/bower_components/bootstrap/dist/css/bootstrap.min.css" />
+	<link rel="stylesheet" type="text/css" href="/static/bower_components/font-awesome/css/font-awesome.min.css" />
+	<link rel="stylesheet" type="text/css" href="/static/bower_components/angular-motion/dist/angular-motion.min.css">
+	<link rel="stylesheet" type="text/css" href="/static/app/css/common.css" />
+	<link rel="stylesheet" type="text/css" href="/static/app/css/order_create.css" />
+	<link rel="shortcut icon" href="/favicon.ico" />
+	<script type="text/javascript" src="/static/bower_components/jquery/dist/jquery.min.js"></script>
+	<script type="text/javascript" src="/static/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="/static/bower_components/angular/angular.min.js"></script>
+	<script type="text/javascript" src="/static/bower_components/angular-cookies/angular-cookies.min.js"></script>
+	<script type="text/javascript" src="/static/bower_components/angular-sanitize/angular-sanitize.min.js"></script>
+	<script type="text/javascript" src="/static/bower_components/angular-strap/dist/angular-strap.min.js"></script>
+	<script type="text/javascript" src="/static/bower_components/angular-strap/dist/angular-strap.tpl.min.js"></script>
+	<script type="text/javascript" src="/static/app/modules/common/i18n.js"></script>
+	<script type="text/javascript" src="/static/app/modules/common/services.js"></script>
+	<script type="text/javascript" src="/static/app/modules/common/directives.js"></script>
+	<script type="text/javascript" src="/static/app/modules/order_create.js"></script>
 </head>
 <body ng-controller="OrderCreateCtrl" class="container-fluid">
 	<nav class="row setting-info">
