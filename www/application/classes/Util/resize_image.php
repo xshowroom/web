5b32ce293322a13dd8f3327deb4a7b4b71ee7aee
arwutang@hotmail.com <arwutang@hotmail.com>
2015-12-18 02:58:02 +0800

    add script

---
+++ www/application/classes/Util/resize_image.php
@@ -0,0 +1,83 @@
+<?php defined('SYSPATH');
+/**
+ * @author xiaotao
+ */
+
+function resize($filename, $percent)
+{
+    // 获取新的尺寸
+    list($width, $height, $type) = getimagesize($filename);
+    $new_width = $width * $percent;
+    $new_height = $height * $percent;
+
+    // 重新取样
+    $image_p = imagecreatetruecolor($new_width, $new_height);
+    switch ($type) {
+        case 1:
+            $image = imagecreatefromgif($filename);
+            break;
+        case 2:
+            $image = imagecreatefromjpeg($filename);
+            break;
+        case 3:
+            $image = imagecreatefrompng($filename);
+            break;
+        default:
+            $image = imagecreatefromjpeg($filename);
+            break;
+    }
+    imagecopyresampled($image_p, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+
+    $output = $filename;
+
+    // 输出
+    imagejpeg($image_p, $output, 100);
+
+    return $output;
+}
+
+
+
+# define variables
+$pattern = $argv[1];
+$targetSize = $argv[2];
+$workingDir =  $argv[3];
+$isProcess =  $argv[4];
+
+echo 'resize pattern: ' . $pattern.', resize target: ' . $targetSize . ', working on: ' . $workingDir;
+
+$dir=opendir($workingDir.".");
+
+//files to process
+$fileList = array();
+
+while (($file = readdir($dir))!= false)
+{
+    $filename = $workingDir. '/' .$file;
+    if (is_file($filename)) {
+        if (preg_match($pattern, $filename)) {
+            $fileSize = filesize($filename);
+
+            if($fileSize > $targetSize * 1024) {
+                $fileList[] = $filename;
+            }
+        }
+    }
+}
+closedir($dir);
+
+echo 'below images will be processed: ';
+print_r($fileList);
+
+foreach($fileList as $processFile) {
+    $fileSize = filesize($processFile);
+
+    if($fileSize > $targetSize * 1024) {
+        if ($isProcess) {
+            echo 'Process: ' . $processFile;
+
+            $resizeImagePath = resize($processFile, 0.5);
+            $imagePath = $resizeImagePath;
+        }
+    }
+}
