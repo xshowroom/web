70d051099cb88a571808193804e0c54b52920278
arwutang <arwutang@hotmail.com>
2015-12-27 12:12:40 +0800

    bug fix

--- www/application/classes/Controller/Xsadmin/Management.php
+++ www/application/classes/Controller/Xsadmin/Management.php
@@ -74,6 +74,18 @@ class Controller_Xsadmin_Management extends Controller_BaseAdmin
         $this->response->body($view);
     }
 
+    public function action_store_detail()
+    {
+        $storeId = $this->request->param('id');
+        var_dump(11111);
+        exit(1);
+        $view = View::factory('store_index');
+        $view->set('storeId', $storeId);
+        $this->response->body($view);
+
+        $this->response->body($view);
+    }
+
     public function action_orders()
     {
         $adminId = $this->adminUser['id'];
