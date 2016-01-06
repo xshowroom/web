dbd838b4d2840a90f4c1585cde7551ae24107c37
Shi-Liang <385936547@qq.com>
2016-01-06 19:48:21 +0800

    bug fix

--- www/application/views/common/global_libraries.php
+++ www/application/views/common/global_libraries.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" type="text/css" href="/static/bower_components/bootstrap/dist/css/bootstrap.min.css" />
 <link rel="stylesheet" type="text/css" href="/static/bower_components/font-awesome/css/font-awesome.min.css" />
-<link rel="stylesheet" type="text/css" href="/static/bower_components/angular-motion/dist/angular-motion.min.css">
+<link rel="stylesheet" type="text/css" href="/static/bower_components/angular-motion/dist/angular-motion.min.css"/>
 <link rel="stylesheet" type="text/css" href="/static/app/css/common.css" />
 <link rel="shortcut icon" href="/favicon.ico" />
 <script type="text/javascript" src="/static/bower_components/jquery/dist/jquery.min.js"></script>
