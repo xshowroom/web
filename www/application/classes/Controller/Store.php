a566afd66e74c139ce6720c68a2baa85057b3b22
arwutang@hotmail.com <arwutang@hotmail.com>
2016-01-14 20:53:28 +0800

    add store link

--- www/application/classes/Controller/Store.php
+++ www/application/classes/Controller/Store.php
@@ -8,7 +8,7 @@ class Controller_Store extends Controller_BaseReqLogin
     public function before()
     {
         parent::before();
-        $this->checkBuyerUser();
+
         $this->userService = new Business_User();
     }
 
@@ -24,6 +24,8 @@ class Controller_Store extends Controller_BaseReqLogin
     
     public function action_create()
     {
+        $this->checkBuyerUser();
+
     	$view = View::factory('store_create');
     	$view->set('user', $this->opUser);
     	$view->set('userAttr', $this->userService->getUserAttr($this->opUser['id']));
