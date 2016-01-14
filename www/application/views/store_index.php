2799c367d031c024b16534781252cde3c086cfa5
arwutang@hotmail.com <arwutang@hotmail.com>
2016-01-14 21:16:16 +0800

    bug fix

--- www/application/views/store_index.php
+++ www/application/views/store_index.php
@@ -16,7 +16,7 @@
 
     <?php if($user["role_type"] == Model_User::TYPE_USER_BRAND): ?>
         <nav class="row user-navigation">
-            <?php echo View::factory('common/global_navigation_top_buyer', array('currentPage' =>  'dashboard', 'userAttr'=> $userAttr)); ?>
+            <?php echo View::factory('common/global_navigation_top_brand', array('currentPage' =>  'dashboard', 'userAttr'=> $userAttr)); ?>
         </nav>
     <?php elseif ($user["role_type"] == Model_User::TYPE_USER_BUYER): ?>
         <nav class="row user-navigation">
