6d0b5b122fbc6c76dc354261ae558d7e28a841c4
Shi-Liang <385936547@qq.com>
2016-01-18 13:09:13 +0800

    规范化后的common js

--- www/static/app/modules/common/i18n.js
+++ www/static/app/modules/common/i18n.js
@@ -1,11 +1,16 @@
+/**
+ * @file i18n filter
+ * @author shiliang - shiliang87@gmail.com
+ *
+ */
 angular.module(
-    'xShowroom.i18n', 
+    'xShowroom.i18n',
     [
         'xShowroom.dictionary'
     ]
 )
-.filter('translate', ['dictionary', function(dictionary){
-	return function(key){
-		return dictionary[key] || 'Error: label is not found';
-	}
+.filter('translate', ['dictionary', function (dictionary) {
+    return function (key) {
+        return dictionary[key] || 'Error: label is not found';
+    };
 }]);
